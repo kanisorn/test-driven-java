74b74b8f3c2e3a9e7c80eaf59752978d30832001
Kanisorn Hongwatcharasakun <kanisorn.hongwatchar@thomsonreuters.com>
2014-10-26 23:08:53 +0700

    [main] update main method

---
+++ src/MainClass.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		DuplicateRemover.getUnique(getDuplicateList());
+		DuplicateRemover.getUnique_1(getDuplicateList());
+	}
+
+	private static List<String> getDuplicateList() {
+		
+		List<String> duplicateList = new ArrayList<String>();
+		duplicateList.add("goofy");
+		duplicateList.add("betty");
+		duplicateList.add("omega");
+		duplicateList.add("betty");
+		duplicateList.add("foto");
+		duplicateList.add("goofy");
+		duplicateList.add("goofy");
+		duplicateList.add("betty");
+		duplicateList.add("omega");
+		duplicateList.add("betty");
+		duplicateList.add("foto");
+		duplicateList.add("goofy");
+		
+		return duplicateList;
+	}
+}
